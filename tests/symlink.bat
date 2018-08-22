64b77665527d695d21cb3a952e7b4d951006a5a7
Neil Popham <neilpopham@gmail.com>
2018-08-22 16:34:56 +0100

    Added comments for use

--- tests/symlink.bat
+++ tests/symlink.bat
@@ -1,4 +1,10 @@
 @ECHO OFF
+
+REM This batch file should be placed in the same folder as tic80.exe
+REM Drag a lua file from the cart folder onto this batch file to create a symlink
+REM and then use dofile("file.lua") in the file.tic file to include that lua file.
+REM This allows us to edit lua in an external editor without affecting sprites, etc.
+
 SET src="%1"
 SET dst="%~nx1"
 MKDIR %src% %dst%
