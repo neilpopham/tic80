faa94aa6276e3c478bd908fea3b5090ce84a3776
Neil Popham <neilpopham@gmail.com>
2018-09-03 17:03:19 +0100

    Testing p8 style lua file

---
+++ tests/test.lua
@@ -0,0 +1,15 @@
+-- title:  test
+-- author: Neil Popham
+-- desc:   Testing p8 style self contained file
+-- script: lua
+-- input:  gamepad
+
+
+-- <TILES>
+-- 001:1111111111111111111111111111111111111111111111111111111111111111
+-- </TILES>
+
+-- <PALETTE>
+-- 000:0000001d2b537e255383769cab5236008751ff004d5f574fff77a8ffa300c2c3c700e436ffccaa29adffffec27fff1e8
+-- </PALETTE>
+
