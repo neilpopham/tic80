7f7f9fee139c4eabe2bce31d14dadf9f6fd2a3d0
Neil Popham <neilpopham@gmail.com>
2018-10-11 15:30:05 +0100

    Finalised...

--- tests/asteriods.lua
+++ tests/asteriods.lua
@@ -96,8 +96,8 @@ function _draw()
     line(
      star.x,
      star.y,
-     star.x+p.dx*p.force/4*star.depth,
-     star.y+p.dy*p.force/4*star.depth,
+     star.x+p.dx*p.force/3*star.depth,
+     star.y+p.dy*p.force/3*star.depth,
      star.col2
     )
    end
